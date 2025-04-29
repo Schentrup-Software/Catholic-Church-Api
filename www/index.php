47072268ced45d6cf4bf713e97e501e16dc3765c
Joey Schentrup <j03y@live.com>
2025-04-29 17:40:37 +0000

    Add basic routing

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PhpApi\Router;
+use PhpApi\Model\RouterOptions;
+use PhpApi\Model\SwaggerOptions;
+use PhpApiSample\Middleware\HeaderCheckMiddleware;
+use PhpApiSample\Middleware\TimeResponseMiddleware;
+
+$router = (new Router(
+    new RouterOptions(
+        namespace: 'SchentrupSoftware\\CatholicChurchApi',
+        directory: __DIR__ . '/../src/Routes',
+    ),
+    new SwaggerOptions(
+        title: "Sample API Documents",
+        apiVersion: "1.2.3",
+    )
+))->addMiddleware(
+    new TimeResponseMiddleware(),
+)->addMiddleware(
+    new HeaderCheckMiddleware(),
+);
+
+$router->route()->send();
